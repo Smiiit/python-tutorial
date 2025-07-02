4c5f0326a68628668fd73c6d12e210da12ac95c7
Smiiit <smitkpanchal03@gmail.com>
2025-07-02 22:07:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# print('Radhe Radhe')
+
+# -Python: In 1980  Guido Van Rossum thought of new language apart from ABC language and then develop the python and launches in 1992 
+
+# -single line comments
+
+'''
+Multi line comments also know as docker string
+'''
+
+# -variables
+# -variables naming ( snake_case , camelCase , PascalCase )
+
+# -Rule : Never start with number (but can include number in btw), Never user Speacial characters, Never give space at beginning and also in btw
+
+# snake_case = 'This is the snake case naming for variables'
+
+# camelCase = 'This is the camelCase naming for variables'
+
+# PascalCase = 'This is the Pascal Case naming for the variables'
